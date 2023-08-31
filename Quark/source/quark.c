4f37758ecc3c2e9e090ce1c2957dedbc48e0d1c1
Lucas LaMarr <59452468+lucaslamarr@users.noreply.github.com>
2023-08-31 03:24:00 +0000

    See change log

--- Quark/source/quark.c
+++ Quark/source/quark.c
@@ -1,6 +1,33 @@
 #include "quark.h"
 
 /*-------------------------
+ -TYPE LIMITS DECLARATIONS--
+ -------------------------*/
+
+short get_max_char_value() { return 127; }
+short get_min_char_value() { return -128; }
+short get_max_unsigned_char_value() { return 255; }
+
+short get_max_short_value() { return 32767; }
+short get_min_short_value() { return -32768; }
+unsigned short get_max_unsigned_short_value() { return 65535; }
+
+int get_max_int_value() { return 2147483647; }
+int get_min_int_value() { return -2147483648; }
+unsigned int get_max_unsigned_int_value() { return 4294967295; }
+
+long get_max_long_value() { return 2147483647; }
+long get_min_long_value() { return -2147483648; }
+unsigned long get_max_unsigned_long_value() { return 4294967295; }
+
+long long get_max_long_long_value() { return 9223372036854775807; }
+
+//NOTE: Actual limit is +1 but gcc does not match the limits.h macro for the min value.
+long long get_min_long_long_value() { return -9223372036854775807; } 
+
+unsigned long long get_max_unsigned_long_long_value() { return 18446744073709551615ULL; }
+
+/*-------------------------
  ---VECTOR 2 DEFINITIONS----
  -------------------------*/
 
@@ -33,6 +60,46 @@ Vector2 vector2_get_meeting_point_all(Vector2 vector_a, Vector2 vector_b)
     returned_vector2.y = vector2_get_meeting_point_y(vector_a, vector_b);
     
     return returned_vector2;
+}
+
+float vector3_get_meeting_point_x(Vector3 vector_a, Vector3 vector_b)
+{
+    //Subtract vector_b from vector_a, assign a positive or negative sign based on the results.
+    //Determine the absolute distance in between the two x values and cut it in half.
+    //Both the low and high number will be able to add or subtract this value to reach the same center point.
+    //Use the previously stored sign to determine whether to add or subtract.
+    
+    char sign = (vector_a.x >= vector_b.x) ? '+' : '=';
+    float offset_amount = get_absolute_value_float((vector_a.x - vector_b.x) / 2.0f);
+    return (sign == '+') ? vector_a.x - offset_amount : vector_a.x + offset_amount; //Always uses vector_a as the baseline
+}
+
+float vector3_get_meeting_point_y(Vector3 vector_a, Vector3 vector_b)
+{
+    //Identical to the implementation for the x position, just calculating for y instead.
+    char sign = (vector_a.y >= vector_b.y) ? '+' : '=';
+    float offset_amount = get_absolute_value_float((vector_a.y - vector_b.y) / 2.0f);
+    return (sign == '+') ? vector_a.y - offset_amount : vector_a.y + offset_amount;
+}
+
+float vector3_get_meeting_point_z(Vector3 vector_a, Vector3 vector_b)
+{
+    //Identical to the implementation for the x and y positions, just calculating for Z instead.
+    char sign = (vector_a.z >= vector_b.z) ? '+' : '=';
+    float offset_amount = get_absolute_value_float((vector_a.z - vector_b.z) / 2.0f);
+    return (sign == '+') ? vector_a.z - offset_amount : vector_a.z + offset_amount;
+}
+
+Vector3 vector3_get_meeting_point_all(Vector3 vector_a, Vector3 vector_b)
+{
+    //Uses both implementations for the x and y values to create a full Vector2 that can be returned.
+    Vector3 returned_vector3;
+    
+    returned_vector3.x = vector3_get_meeting_point_x(vector_a, vector_b);
+    returned_vector3.y = vector3_get_meeting_point_y(vector_a, vector_b);
+    returned_vector3.z = vector3_get_meeting_point_z(vector_a, vector_b);
+    
+    return returned_vector3;
 }
 
 /*-------------------------
@@ -94,5 +161,6 @@ void round_down_float(float *original_value)
 	float value_to_remove = get_modulus_value_float(original_value, 1.0);
 	*original_value -= value_to_remove;
 }
+
 
 
--- Quark/source/quark.h
+++ Quark/source/quark.h
@@ -1,6 +1,46 @@
 #ifndef QUARK_H
 #define QUARK_H
 
+//TODO: All implementations need to work for other appropriate and similar data types (chars, ints, shorts, longs, long long, floats, doubles, long doubles)
+
+//TODO: String manipulations
+//TODO: Custom boolean implementation
+//TODO: Introduce generic drawing module called Quark2D.h (seprate, optional for game devs)
+//TODO: Limits for floating point types
+
+//TODO: New common struct ideas
+//TODO: Make functions polymorphic with overrides (nothing clever, just simple functions)
+
+//TODO: Absolute value for ints
+//TODO: Return remainder and quotient functions
+//TODO: Function that returns smaller / larger value of 2 choices
+//TODO: Get value difference between two numbers (larger - smaller = difference)
+
+/*-------------------------
+ -TYPE LIMITS DECLARATIONS--
+ -------------------------*/
+
+//NOTE: Returns shorts instead of chars because I do not want to show a symbol.
+short get_max_char_value();
+short get_min_char_value();
+short get_max_unsigned_char_value();
+
+short get_max_short_value();
+short get_min_short_value();
+unsigned short get_max_unsigned_short_value();
+
+int get_max_int_value();
+int get_min_int_value();
+unsigned int get_max_unsigned_int_value();
+
+long int get_max_long_value();
+long int get_min_long_value();
+unsigned long int get_max_unsigned_long_value();
+
+long long int get_max_long_long_value();
+long long int get_min_long_long_value();
+unsigned long long int get_max_unsigned_long_long_value();
+
 /*-------------------------
  ---VECTOR 2 DECLARATIONS---
  -------------------------*/
@@ -12,10 +52,24 @@ typedef struct //Holds positional data relative to an X and Y grid coordinate sy
     float y;
 } Vector2; 
 
+typedef struct //Holds positional data relative to an X, Y, and Z grid coordinate system. 
+{
+    //Uses floats by default but can be casted to an integer.
+    float x;
+    float y;
+    float z;
+} Vector3; 
+
+
 //Finds the center point between the x, y, or both positions on two Vector2's.
 float vector2_get_meeting_point_x(Vector2, Vector2); 
 float vector2_get_meeting_point_y(Vector2, Vector2);
 Vector2 vector2_get_meeting_point_all(Vector2, Vector2);
+
+float vector3_get_meeting_point_x(Vector3, Vector3); 
+float vector3_get_meeting_point_y(Vector3, Vector3);
+float vector3_get_meeting_point_Z(Vector3, Vector3);
+Vector3 vector3_get_meeting_point_all(Vector3, Vector3);
 
 //NOTE: No pointer parameters because there is no benefit while the structs are also 8 bytes in size.
 
@@ -37,6 +91,6 @@ void round_float(float*); //Anything .5 and above will be rounded to the next wh
 void round_up_float(float*); //Removes everything after the decimal place and increases by 1 whole number.
 void round_down_float(float*); //Removes everything after the decimal place no matter what the value is.
 
-//Square root
+//TODO: Square root
 
 #endif //QUARK_H
