8d5be47209e932d07bb8eb5bfc08f9624b9e05b1
Lucas LaMarr <59452468+lucaslamarr@users.noreply.github.com>
2023-12-16 16:11:58 -0500

    Add files via upload

---
+++ Quark Lib/source/quark.h
@@ -0,0 +1,13 @@
+#ifndef QUARK_H
+#define QUARK_H
+
+#include "quarkcore.h"
+#include "quarkmath.h"
+#include "quarkgraphics.h"
+
+/*
+ #include "quarkds.h"
+ #include "quarkalgo.h"
+*/
+
+#endif
---
+++ Quark Lib/source/quarkgraphics.h
@@ -0,0 +1,35 @@
+#ifndef QUARKGRAPHICS_H
+#define QUARKGRAPHICS_H
+
+#include "quarkcore.h"
+
+bool is_running;
+
+/*-------------------------
+ ------WINDOW CREATION------
+ -------------------------*/
+
+typedef struct 
+{
+	const char* window_title;
+	int window_width;
+	int window_height;
+} Window;
+
+/* Import the appropriate files depending on the current platform */
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+/* TODO: MacOS */
+/* TODO: Linux */
+
+HWND window_handle;
+
+#endif
+
+bool create_window(Window window);
+void window_update();
+
+#endif
